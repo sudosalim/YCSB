15a49be92027f7bdf6769d05c5fe34a118fcfb92
Alex Gyryk <oleksandr.gyryk@couchbase.com>
2017-08-30 13:05:43 -0700

    soe2 initial commit

---
+++ core/src/main/java/com/yahoo/ycsb/generator/soe/PredicateGenerator.java
@@ -0,0 +1,9 @@
+package com.yahoo.ycsb.generator.soe;
+
+import com.yahoo.ycsb.generator.Generator;
+
+/**
+ * Created by oleksandr.gyryk on 8/30/17.
+ */
+public class PredicateGenerator  {
+}
---
+++ core/src/main/java/com/yahoo/ycsb/workloads/soe/SoeWorkload.java
@@ -0,0 +1,9 @@
+package com.yahoo.ycsb.workloads.soe;
+
+import com.yahoo.ycsb.workloads.CoreWorkload;
+
+/**
+ * Created by oleksandr.gyryk on 8/30/17.
+ */
+public class SoeWorkload extends CoreWorkload {
+}
