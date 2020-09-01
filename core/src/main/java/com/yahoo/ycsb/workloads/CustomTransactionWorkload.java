32480af0828560eb865d3b1cce9508db7a3e9d18
sharath chandra <sharujayaram@gmail.com>
2020-09-01 15:01:24 -0700

    updating transactionoperationchooser to choose new value every occurence

--- core/src/main/java/com/yahoo/ycsb/workloads/CustomTransactionWorkload.java
+++ core/src/main/java/com/yahoo/ycsb/workloads/CustomTransactionWorkload.java
@@ -90,12 +90,12 @@ public class CustomTransactionWorkload extends CoreWorkload {
     HashSet<String> fields = null;
     HashMap<String, ByteIterator> result = new HashMap<String, ByteIterator>();
 
-    String transactionOperation = transactionoperationchooser.nextString();
+    //String transactionOperation = transactionoperationchooser.nextString();
 
     for(int i=0; i<documentsintransaction; i++){
-      transationOperations[i] = transactionOperation;
+      transationOperations[i] = transactionoperationchooser.nextString();
 
-      switch (transactionOperation) {
+      switch (transationOperations[i]) {
       case "TRREAD":
         transationKeys[i] = buildKeyName(nextKeynum());
         transationValues[i] = null;
